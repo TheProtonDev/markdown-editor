9ed29cb160f7486f47f5d62777ebae96d23d4f68
TheProtonDev <47036393+TheProtonDev@users.noreply.github.com>
2022-07-21 22:24:59 -0600

    V0.2 Slightly more responsive CSS, tolerable experience on any device with 900 or more pixels in width, otherwise site breaks on purpose due to mobile support not being implemented yet Textarea and preview iframe are now same size Added h3, h4, h5, h6, and syntax error message if parser method fails for some reason

--- assets/script.js
+++ assets/script.js
@@ -25,25 +25,69 @@ const alphabet_and_special_chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQR
 const alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const splitLines = str => str.split(/\r?\n/);
 
+function isBolded(markdownCode){
+    // TODO: Get this to work as expected upon the input of 2 or more bolded words
+    let asterisksFound = []
+    for (let charIndex in markdownCode){
+        let char = markdownCode[charIndex];
+        if (char === "*"){
+            asterisksFound.push(charIndex)
+        }
+    }
+    let numToAdd = 0
+    for (let i = 2; i < asterisksFound.length / 2; i++){
+        if (asterisksFound.length % 4 === 0){
+            // Get the position of the first actual character after being prefaced with **
+            let boldBorder1 = asterisksFound[1 + numToAdd]
+            // Get the position of the last actual character after being closed with **
+            let boldBorder2 = asterisksFound[3 + numToAdd]
+
+            let textToBeBolded = markdownCode.slice(boldBorder1, boldBorder2)
+            textToBeBolded = textToBeBolded.slice(1, textToBeBolded.length - 1)
+
+            console.log(textToBeBolded)
+            numToAdd++
+        }
+        console.log(asterisksFound)
+    }
+}
+
+isBolded("# **Test** 2 **bold2**")
+
 function parseMarkdown(markdownCode) {
     let isHeader1 = markdownCode.slice(0, 2) === '# ' && alphabet_and_special_chars.search(markdownCode[1]) !== -1
     let isHeader2 = markdownCode.slice(0, 3) === '## ' && alphabet_and_special_chars.search(markdownCode[2]) !== -1
+    let isHeader3 = markdownCode.slice(0, 4) === '### ' && alphabet_and_special_chars.search(markdownCode[3]) !== -1
+    let isHeader4 = markdownCode.slice(0, 5) === '#### ' && alphabet_and_special_chars.search(markdownCode[4]) !== -1
+    let isHeader5 = markdownCode.slice(0, 6) === '##### ' && alphabet_and_special_chars.search(markdownCode[5]) !== -1
+    let isHeader6 = markdownCode.slice(0, 7) === '###### ' && alphabet_and_special_chars.search(markdownCode[6]) !== -1
     let isPlaintext = alphabet_and_special_chars.search(markdownCode[0]) !== -1
     let isEmptyLine = markdownCode === ""
     let finalParser = {
         "header1": isHeader1,
         "header2": isHeader2,
+        "header3": isHeader3,
+        "header4": isHeader4,
+        "header5": isHeader5,
+        "header6": isHeader6,
         "isPlaintext": isPlaintext,
         "isEmptyLine": isEmptyLine
     }
-    console.log(finalParser)
     switch (true) {
         case (finalParser.header1):
             return `<h1>${markdownCode.slice(2, markdownCode.length)}</h1>`
         case (finalParser.header2):
             return `<h2>${markdownCode.slice(2, markdownCode.length)}</h2>`
+        case (finalParser.header3):
+            return `<h3>${markdownCode.slice(3, markdownCode.length)}</h3>`
+        case (finalParser.header4):
+            return `<h4>${markdownCode.slice(4, markdownCode.length)}</h4>`
+        case (finalParser.header5):
+            return `<h5>${markdownCode.slice(5, markdownCode.length)}</h5>`
+        case (finalParser.header6):
+            return `<h6>${markdownCode.slice(6, markdownCode.length)}</h6>`
         case (finalParser.isPlaintext):
-            return `${markdownCode} <br>`
+            return `<p>${markdownCode}</p> <br>`
         case (finalParser.isEmptyLine):
             return `<br>`
     }
@@ -57,7 +101,17 @@ function parseChanges() {
     let newMarkdown = [];
     for (let line in toParse) {
         line = toParse[line];
-        newMarkdown.push(parseMarkdown(line));
+
+        let parseResult = parseMarkdown(line);
+        if (parseResult === undefined) {
+            newMarkdown.push("<p style='color: red;'>" +
+                "Syntax Error. Please make sure you're following <a href='https://www.markdownguide.org" +
+                "/basic-syntax'>Markdown Best Practices.</a> <a href='https://github.com/TheProtonDev/markdown-editor" +
+                "/issues/new?title=Syntax%20Error%20Report'><br>If you confirm that you are, please report a detailed issue submission on GitHub" +
+                "</a></p>");
+        } else {
+            newMarkdown.push(parseResult);
+        }
     }
     updatePreview(newMarkdown);
 }
